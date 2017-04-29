b172985d112e7167016ff106c1385170d76b11fe
kareem2048 <karim.salah2048@gmail.com>
2017-04-29 13:49:54 +0200

    add some basic flightAdapter and modify somedata

--- app/src/main/java/com/p4f/kareem/rad_eye_v2/availableFlights.java
+++ app/src/main/java/com/p4f/kareem/rad_eye_v2/availableFlights.java
@@ -6,8 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-
+//
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -23,7 +24,12 @@ public class availableFlights extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_available_flights, container, false);
+        View view = inflater.inflate(R.layout.fragment_available_flights, container, false);
+        ListView listView = new ListView(getContext());
+
+        //TODO
+        listView.setAdapter(null);
+        return view;
     }
 
 
