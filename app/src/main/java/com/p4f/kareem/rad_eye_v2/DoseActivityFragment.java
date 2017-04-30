6e05a1c5e7cd3e959bd8053ee4c08e43005a14de
kareem2048 <karim.salah2048@gmail.com>
2017-04-30 08:51:10 +0200

    polish the design

--- app/src/main/java/com/p4f/kareem/rad_eye_v2/DoseActivityFragment.java
+++ app/src/main/java/com/p4f/kareem/rad_eye_v2/DoseActivityFragment.java
@@ -1,5 +1,6 @@
 package com.p4f.kareem.rad_eye_v2;
 
+import android.annotation.SuppressLint;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,12 +16,13 @@ public class DoseActivityFragment extends Fragment {
     public DoseActivityFragment() {
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         String dose = getActivity().getIntent().getExtras().getString("dose");
         View view  = inflater.inflate(R.layout.fragment_dose, container, false);
-        ((TextView) view.findViewById(R.id.currentDose)).setText(dose);
+        ((TextView) view.findViewById(R.id.currentDose)).setText(dose + "\nmSv");
         return view;
     }
 }
